9ae453bae3a563e8641d28d0c4470ccd18cf87aa
Deivid Braian <deividbraian5@gmail.com>
2022-12-27 21:49:34 -0300

    cubic bezier com func

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ function setup() {
   createCanvas(600, 600);
   p0 = createVector(0,300);
   p1 = createVector(300,0);
-  p2 = createVector(600,300);
+  p2 = createVector(400,0);
+  p3 = createVector(600,300);
 }
 
 function draw() {
@@ -12,22 +13,16 @@ function draw() {
   stroke(255);
 
   noFill();
-  beginShape();
   p1.x = mouseX;
   p1.y = mouseY;
   let delta = 0.05;
   colorMode(HSB)
   for (let t = 0; t <= 1.01; t += delta) {
-    let x1 = lerp(p0.x, p1.x, t);
-    let y1 = lerp(p0.y, p1.y, t);
-    let x2 = lerp(p1.x, p2.x, t);
-    let y2 = lerp(p1.y, p2.y, t);
-    stroke(t*360,255,255);
-    line(x1, y1, x2, y2);
-    let x = lerp(x1, x2, t);
-    let y = lerp(y1, y2, t);
+    
+    stroke(t*360,255,255,.6);
+    let v = cubic(p0,p1,p2,p3,t);
+    vertex(v.x, v.y);
   }
-  endShape();
 
   // beginShape();
   // bezierVertex(mouseX, mouseY, 400, 400, 600, 300);
@@ -37,4 +32,26 @@ function draw() {
   
 
 } 
+function cubic(p0, p1, p2,p3,t) {
+  // let x = (1 - t) * (1 - t) * (1 - t) * p0.x + 3 * (1 - t) * (1 - t) * t * p1.x + 3 * (1 - t) * t * t * p2.x + t * t * t * p3.x;
+  // let y = (1 - t) * (1 - t) * (1 - t) * p0.y + 3 * (1 - t) * (1 - t) * t * p1.y + 3 * (1 - t) * t * t * p2.y + t * t * t * p3.y;
+  let v1 = quadratic(p0, p1, p2, t);
+  let v2 = quadratic(p1, p2, p3, t);
+  let x = lerp(v1.x, v2.x, t);
+  let y = lerp(v1.y, v2.y, t);
+  line(v1.x, v1.y, v2.x, v2.y);
+  return createVector(x, y);
+}
+function quadratic(p0, p1, p2, t) {
+  // let x = (1 - t) * (1 - t) * p0.x + 2 * (1 - t) * t * p1.x + t * t * p2.x;
+  // let y = (1 - t) * (1 - t) * p0.y + 2 * (1 - t) * t * p1.y + t * t * p2.y;
+  let x1 = lerp(p0.x, p1.x, t);
+  let y1 = lerp(p0.y, p1.y, t);
+  let x2 = lerp(p1.x, p2.x, t);
+  let y2 = lerp(p1.y, p2.y, t);
+  let x = lerp(x1, x2, t);
+  let y = lerp(y1, y2, t);
+  line(x1, y1, x2, y2);
+  return createVector(x, y);
+}
 
